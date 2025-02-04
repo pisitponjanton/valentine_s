5e49571af1f2c735ff00a4022a4b15f68d9c84ca
pisitponjanton <pisitponjan223@gmail.com>
2025-02-04 07:34:56 +0700

    update

--- onlyforyou/app/layout.js
+++ onlyforyou/app/layout.js
@@ -11,7 +11,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`bg-[#FF5D80] overflow-hidden`}
+        className={`bg-[#FF5D80]`}
       >
         <ThemeProvider>{children}</ThemeProvider>
       </body>
